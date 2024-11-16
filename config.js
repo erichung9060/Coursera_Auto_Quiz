af9ac1b311621e942f87b8b8ea354b83e5c9611f
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-11-16 16:05:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const API_KEY = 'Your_Gooele_Gemini_API_Key'
 const DB_API_URL = "Your_DB_API_URL"
 const DB_PASSWORD = "Your_DB_Password"
-const userId = yourid //yourid save with int, DO NOT save with string.;
+const userId = 1234 // change 1234 to your userid, save with int, DO NOT save with string.
 
 // Get userid: 
 // Enter window.App.context.dispatcher.stores.ApplicationStore.userData.id in your browser console
